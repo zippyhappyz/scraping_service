92c2e4d645cbb965241e5142655713730c998be5
ilyas <askarjan.15.14@gmail.com>
2023-02-15 17:06:31 +0600

    Добавил несколько моделей: Городов, Вакансии и Языка. Сделал функицю отображения вакансии в html файле.

---
+++ src/scraping/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Vacancy
+
+
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home.html', {'object_list' : qs})
--- src/scraping_service/urls.py
+++ src/scraping_service/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from scraping.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('home/', home_view),
 ]
